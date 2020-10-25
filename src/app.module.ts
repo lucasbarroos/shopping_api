ace46bc25892da5a7f094d607c948230fe16b45c
VickLucas <lbarros.comp@gmail.com>
2020-10-25 06:28:54 -0300

    Removing the default app settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './App/app.controller';
-import { AppService } from './App/app.service';
 import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  controllers: [UsersController],
 })
 export class AppModule {}
